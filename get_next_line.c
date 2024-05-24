7a2fe04f11d09c0e46c2e62cd3cb8c7271be566b
Takumasa Matsuura <tamatsuu@student.42tokyo.jp>
2024-05-25 04:35:27 +0900

    modify some code for gnl.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: tamatsuu <tamatsuu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 22:06:27 by tamatsuu          #+#    #+#             */
-/*   Updated: 2024/05/25 02:02:30 by tamatsuu         ###   ########.fr       */
+/*   Updated: 2024/05/25 04:32:41 by tamatsuu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
@@ -22,59 +23,70 @@ char	*get_next_line(int fd)
 	{
 		status.current = 0;
 		status.read_p = 0;
+		status.read_ret = 0;
 		status.fd = fd;
 	}
-	return (read_file(fd, status));
+	//printf("status.read_p:%lu status.current:%lu  BUFFER_SIZE:%d\n",status.read_p, status.current,  BUFFER_SIZE);
+	return (read_file(&status));
 }
 
-char	*read_file(int fd, t_status status)
+char	*read_file(t_status *status)
 {
 	ssize_t	i;
 	char	*tmp;
 
-	if (status.read_p == 0)
-		tmp = NULL;
-	if (status.read_p <= status.current)
+	tmp = NULL;
+	while (1)
 	{
-		status.read_p = read(status.fd, status.buffer, BUFFER_SIZE);
-		if (status.read_p < 0)
+		if (status->read_p <= status->current)
 		{
-			free(tmp);
-			return (NULL);
+			status->read_ret = read(status->fd, status->buffer, BUFFER_SIZE);
+			status->read_p = status->read_ret + status->read_p;
+			if (status->read_ret < 0)
+				return (free(tmp), NULL);
 		}
+		i = status->current;
+	//	printf("status->read_p:%lu status->current:%lu i:%lu tmp:%s BUFFER_SIZE:%d\n",status->read_p, status->current, i, tmp, BUFFER_SIZE);
+		while (status->read_p > status->current)
+		{
+			if (status->buffer[status->current++] == '\n')
+				return (output_line(i, *status, tmp));
+		}
+		tmp = output_line(i, *status, tmp);
+		if (!tmp || status->read_ret == 0)
+			return (tmp);
 	}
-	i = status.current;
-	while (status.read_p != 0 && status.read_p > status.current)
-	{
-		if (status.buffer[status.current++] == '\n')
-			return (output_line(i, status, tmp));
-	}
-	tmp = output_line(i, status, tmp);
-	if (!tmp)
-		return (NULL);
 }
 
 //like a substr + strjoin
 char	*output_line(ssize_t i, t_status status, char *tmp)
 {
 	char	*ret;
+	char	*tmp_2;
 
 	if (!tmp)
-		return (ft_strndup(status.buffer[i], status.current - i));
-	ret = ft_strjoin(tmp, ft_strndup(status.buffer[i], status.current - i));
-	if (!ret)
+		return (ft_strndup(&(status.buffer[i]), status.current - i));
+	tmp_2 = ft_strndup(&(status.buffer[i]), status.current - i);
+	if (!tmp_2)
 	{
 		free(tmp);
 		return (NULL);
 	}
+	ret = ft_strjoin(tmp, tmp_2);
+	free(tmp);
+	free(tmp_2);
 	return (ret);
 }
 
 #include <fcntl.h>
+#include <stdio.h>
 int	main(void)
 {
 	int	fd;
 
-	open("./test.txt", O_RDONLY,755);
-
+	fd = open("./test.txt", O_RDONLY,755);
+	printf("your result:%s\n",get_next_line(fd));
+	printf("your result:%s\n",get_next_line(fd));
+	printf("your result:%s\n",get_next_line(fd));
+	close(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,15 @@
 /*   By: tamatsuu <tamatsuu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 22:06:45 by tamatsuu          #+#    #+#             */
-/*   Updated: 2024/05/25 01:59:13 by tamatsuu         ###   ########.fr       */
+/*   Updated: 2024/05/25 04:00:39 by tamatsuu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# define BUFFER_SIZE 10000
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 100
+# endif
 # include <unistd.h>
 # include <stdlib.h>
 
@@ -23,11 +24,13 @@ typedef struct s_status
 	ssize_t	current;
 	ssize_t	read_p;
 	ssize_t	fd;
+	ssize_t	read_ret;
 }	t_status;
 
-char	*read_file(int fd, t_status status);
+char	*read_file(t_status *status);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strndup(char *s, size_t len);
+char	*output_line(ssize_t i, t_status status, char *tmp);
 
 #endif
